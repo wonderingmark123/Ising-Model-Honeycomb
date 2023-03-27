878ab309780b1b18c7dc0ebd2e8fcac858e7e142
wonderingmark123 <490785554@qq.com>
2023-03-27 21:43:56 +0800

    Added the lattice defect and explaination

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Ising-Model-Honeycomb
- Ising model simulation for Honeycomb
+ Ising model simulation for honeycomb and triangular lattice. This code is originally from https://www.mathworks.com/matlabcentral/fileexchange/62194-ising-model-and-metropolis-algorithm for square lattice.
+
+# Usage
+
+The matlab live script files (.mlx) are example to run the code for different kinds of lattice (Triangular, honeycomb and square lattice). 
+If you want to simulate the lattice with impurity and defect lattice, replace initSpins with initSpinsImpurity in your simulation.
+
+# Results
+
+
+
+
+
+
+
--- energyIsingBrick.m
+++ energyIsingBrick.m
@@ -4,8 +4,8 @@ function Emean = energyIsingBrick(spin, J)
 %   configuration |spin|. |spin| is a matrix of +/- 1's. |J| is a scalar.
 
 %   Copyright 2017 The MathWorks, Inc.
-sumOfNeighbors = 0;
-for linearIndex = length(spin)
+sumOfNeighbors = zeros(size(spin));
+for linearIndex = 1:numel(spin)
     [row, col]  = ind2sub(size(spin), linearIndex);
 
     % Find its nearest neighbors
@@ -16,13 +16,11 @@ for linearIndex = length(spin)
     right = mod(col + 1 - 1, size(spin,2)) + 1;
 
     if mod(row+col,2) == 0
-        sumOfNeighbors = sumOfNeighbors + ...
-                        sum([spin(above,col);
+        sumOfNeighbors(row,col) = sum([spin(above,col);
                             spin(row,left);
                             spin(below,col)]);
     else
-        sumOfNeighbors =  sumOfNeighbors + ...
-                        sum([spin(above,col);
+        sumOfNeighbors(row,col) = sum([spin(above,col);
                             spin(row,right);
                             spin(below,col)]);
     end
@@ -30,4 +28,4 @@ end
 
 Em = - J * spin .* sumOfNeighbors;
 E  = 0.5 * sum(Em(:));
-Emean = E / numel(spin);
+Emean = E / sum(abs(sign(spin(:))));
